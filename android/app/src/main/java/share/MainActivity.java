b6ca2e472f5a7ed384bf358bd84e5645dbe30f00
Weixing Sun <weixingsun@VBElCapitan.local>
2016-03-10 00:00:22 +1300

    first commit

---
+++ android/app/src/main/java/share/MainActivity.java
@@ -0,0 +1,32 @@
+package com.share;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.AirMaps.AirPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName(){
+    return "Share";
+  }
+
+  @Override
+  protected boolean getUseDeveloperSupport(){
+    return BuildConfig.DEBUG;
+  }
+
+  @Override 
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+      new MainReactPackage(),
+      new AirPackage(),
+      new VectorIconsPackage()
+    );
+  }
+}
