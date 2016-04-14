bd00ee31aa3ac470dbcc57716db1d7274a30e076
Weixing Sun <weixing.sun@gmail.com>
2016-04-14 22:02:02 +1200

    add autoupdater

--- android/app/src/main/java/com/share/MainActivity.java
+++ android/app/src/main/java/com/share/MainActivity.java
@@ -4,6 +4,7 @@ import co.apptailor.googlesignin.RNGoogleSigninModule;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactActivity;
+import com.aerofs.reactnativeautoupdater.ReactNativeAutoUpdaterPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -15,7 +16,7 @@ import com.keyee.datetime.*;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactNativeAutoUpdaterActivity { //ReactActivity {
 
   @Override
   protected String getMainComponentName(){
@@ -26,18 +27,45 @@ public class MainActivity extends ReactActivity {
   protected boolean getUseDeveloperSupport(){
     return BuildConfig.DEBUG;
   }
+ /**
+  *  Name of the JS Bundle file shipped with the app.
+  *  This file has to be added as an Android Asset.
+  * */
+  @Nullable
+  @Override
+  protected String getBundleAssetName() {
+    return "main.android.jsbundle";
+  }
+
+  /**
+   *  URL for the metadata of the update.
+   * */
+  @Override
+  protected String getUpdateMetadataUrl() {
+    return "https://www.aerofs.com/u/8691535/update.android.json";
+  }
+
+  /**
+   * Name of the metadata file shipped with the app.
+   * This metadata is used to compare the shipped JS code against the updates.
+   * */
+  @Override
+  protected String getMetadataAssetName() {
+    return "metadata.android.json";
+  }
 
   @Override 
   protected List<ReactPackage> getPackages() {
     return Arrays.<ReactPackage>asList(
       new MainReactPackage(),
+      new ReactNativeAutoUpdaterPackage(),
       new ReactNativeLocalizationPackage(),
-      new RNGoogleSigninPackage(this),
       new RCTDateTimePickerPackage(this),
-      new WeChatPackage(),
-      new AirPackage(),
+      new RNGoogleSigninPackage(this),
       new FacebookLoginPackage(),
+      new WeChatPackage(),
       new VectorIconsPackage()
+      new AirPackage(),
     );
   }
   // for google login
