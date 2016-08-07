68ad06197860c50f2b5b723a10f1d6fefa1697b8
Weixing Sun <weixing.sun@gmail.com>
2016-08-07 17:55:18 +1200

    ui change in usb chat page

--- ui/UsbChat.js
+++ ui/UsbChat.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import {ListView, View, Text,TextInput, StyleSheet, ScrollView, TouchableOpacity, } from 'react-native'
 import {Icon} from './Icon'
 import Style from './Style'
+import Global from '../io/Global'
 import NavigationBar from 'react-native-navbar'
 import UsbSerial from 'react-native-usbserial';
 import Button from 'apsl-react-native-button'
@@ -18,7 +19,41 @@ export default class APIList extends React.Component {
           to:'000000',
           from:'000100',
           msgList:[],
+          gps:true,
+          pos:{},
       };
+      //speed,heading,accuracy,longitude,latitude
+    }
+    turnOnGps(){
+      let self=this
+      if(Global.MAP===Global.GoogleMap){
+        this.watchID = navigator.geolocation.watchPosition((position) => {
+            //{timestamp,{coords:{speed,heading,accuracy,longitude,latitude,altitude}}}
+            self.setState({pos:position.coords})
+            //self.sendDataToUsbSerialDevice(JSON.stringify(self.pos));
+          },
+          (error) => console.log(error.message),
+          {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000, distanceFilter:30},
+        );
+      }else if(Global.MAP===Global.BaiduMap){
+        this.watchID = KKLocation.watchPosition((position) => {
+            //{timestamp,{coords:{heading,accuracy,longitude,latitude}}}  //no speed,altitude
+            self.setState({pos:position.coords})
+            //self.sendDataToUsbSerialDevice(JSON.stringify(self.pos));
+        });
+      }
+      this.setState({gps:true});
+    }
+    turnOffGps(){
+      navigator.geolocation.clearWatch(this.watchID);
+      this.setState({gps:false});
+    }
+    switchGps(){
+      if(this.state.gps) {
+          this.turnOffGps();
+      }else{
+          this.turnOnGps();
+      }
     }
     componentWillMount(){
         UsbSerial.listen(this.props.usb.device, 9600, '}', (e)=>{ //0x means send in hex format: p2p mode
@@ -31,18 +66,26 @@ export default class APIList extends React.Component {
                 //alert(JSON.stringify(e.attached))
             }
         });
+        this.turnOnGps();
     }
     componentWillUnmount(){
         UsbSerial.close();
     }
-    sendMsgToUsb(){
+    sendDataToUsb(data){
+        UsbSerial.write(this.state.to, this.state.from, '{'+data+'}');
+        let arr = this.state.msgList
+        let newMsg = {send:1,to:this.state.to,data:data}
+        arr.push(newMsg)
+        this.setState({msgList:arr,text:''})
+    }
+    sendMsg(){
         if(this.state.text.length>0){
-            UsbSerial.write(this.state.to, this.state.from, '{'+this.state.text+'}');
-            let arr = this.state.msgList
-            let newMsg = {send:1,to:this.state.to,data:this.state.text}
-            arr.push(newMsg)
-            this.setState({msgList:arr,text:''})
+            this.sendDataToUsb(this.state.text)
         }
+    }
+    sendGps(){
+        let latlng = this.pos.latitude.toFixed(6)+','+this.pos.longitude.toFixed(6)
+        UsbSerial.write(this.state.to, this.state.from, '{'+latlng+'}');
     }
     rowLeftBlankView(json){
         if(json.send==1)
@@ -53,6 +96,7 @@ export default class APIList extends React.Component {
             return <View style={{flex:1}}/>
     }
     _renderRow(data, sectionID, rowID) {
+        let bgColor = data.send==1?'#ef553a':'#32ED42'
         return (
           <View style={{height: 44,flexDirection:'row'}} >
               {this.rowLeftBlankView(data)}
@@ -61,7 +105,7 @@ export default class APIList extends React.Component {
                   height: 40,
                   borderWidth: 1,
                   borderColor: '#AAAAAA',
-                  backgroundColor:'#ef553a',
+                  backgroundColor: bgColor,
                   borderRadius:10,
                   }}>
                   <Text style={{fontSize:18,color:'white'}}>{data.data}</Text>
@@ -74,6 +118,7 @@ export default class APIList extends React.Component {
     render(){
       //if(this.api_list.length===0) return <Loading />
       //alert(JSON.stringify(this.api_list))
+      let gps_color = this.state.pos.latitude?'black':'gray'
       return (
       <View>
           <NavigationBar style={Style.navbar} title={{title:this.props.usb.vendor+': '+this.props.usb.product,}} 
@@ -105,8 +150,14 @@ export default class APIList extends React.Component {
               />
               <Button 
                   style={{backgroundColor:'#3498db',borderColor:'#2980b9',width:80,height:Style.NAVBAR_HEIGHT-30}}
-                  onPress={()=>this.sendMsgToUsb()}
+                  onPress={()=>this.sendMsg()}
               >Send</Button>
+              <Button
+                  style={{backgroundColor:'#3498db',borderColor:'#2980b9',width:80,height:Style.NAVBAR_HEIGHT-30}}
+                  onPress={()=>this.sendGps()}
+              >
+                  <Icon name={"ion-ios-navigate-outline"} color={gps_color} size={30} />
+              </Button>
           </View>
       </View>
       );
