8a4b78feef073417447b2a29651abb66ef9f1764
Weixing Sun <weixing.sun@gmail.com>
2016-10-30 10:25:24 +1300

    upgrade to RN.36

--- android/app/src/main/java/com/share/MainActivity.java
+++ android/app/src/main/java/com/share/MainActivity.java
@@ -3,12 +3,13 @@ package com.share;
 import com.burnweb.rnpermissions.RNPermissionsPackage;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.app.Activity;
 
-public class MainActivity extends ReactActivity { //implements ActivityEventListener {
+public class MainActivity extends ReactActivity { // implements ActivityEventListener {
 
     private static final String LOG_TAG = "MainActivity";
 
@@ -23,28 +24,16 @@ public class MainActivity extends ReactActivity { //implements ActivityEventList
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.e("MainActivity.onActivityResult(req,res,intent)", "requestCode="+requestCode+" resultCode="+resultCode );
+    /*@Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }*/
+    @Override  // >33
+    public void onActivityResult(int requestCode, int resultCode, Intent data) { //Activity activity, 
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
-    /*@Override  // >33
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-        Log.e("MainActivity.onActivityResult(act,req,res,intent):", "requestCode="+requestCode+" resultCode="+resultCode );
-        //super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-    }
-    
-    @Override
-    public void onNewIntent(Intent intent) {
-        this.setIntent(intent);
-        Bundle bundle = intent.getExtras();
-        for (String key : bundle.keySet()) {
-            Object value = bundle.get(key);
-            String log = String.format("%s %s (%s)", key, value.toString(), value.getClass().getName());
-            Log.e("MainActivity.onNewIntent()", log );
-        }
-    }
-    */
+    @Override  // >33
+    public void onNewIntent(Intent intent) {}
 }
